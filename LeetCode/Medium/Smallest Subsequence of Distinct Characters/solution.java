87b400c12ca3f51f1d58e3e466cd41765402c979
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-05-15 16:21:06 +0530

    Solved Smallest Subsequence of Distinct Characters

---
+++ LeetCode/Medium/Smallest Subsequence of Distinct Characters/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+         int[] freq=new int[26];
+        for(char ch:s.toCharArray())
+           freq[ch-'a']++;
+        boolean[] visited=new boolean[26];
+        Stack<Character> stack=new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(visited[ch-'a']){
+                freq[ch-'a']--;
+                continue;
+            }
+            while(!stack.isEmpty() && ch<stack.peek() && freq[stack.peek()-'a']>1){
+                freq[stack.peek()-'a']--;
+                visited[stack.pop()-'a']=false;
+            }
+            stack.push(ch);
+            visited[ch-'a']=true;
+        }
+        StringBuilder result=new StringBuilder();
+        for(char ch:stack){
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
