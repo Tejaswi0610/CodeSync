a4092e76f234fa434cd9fc2f3e42edb604fc88d5
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-05-16 19:18:23 +0530

    Solved Sum Tree | Practice | GeeksforGeeks

---
+++ GeeksForGeeks/Medium/Sum Tree  Practice  GeeksforGeeks/solution.java
@@ -0,0 +1,32 @@
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    int sum(Node root){
+        if(root==null) return 0;
+        if(root.left==null && root.right==null) return root.data;
+        
+        int left=sum(root.left);
+        if(left==-1) return -1;
+        
+        int right=sum(root.right);
+        if(right==-1) return -1;
+        
+        if(root.data!=left+right) return -1;
+        
+        return root.data+left+right;
+    }
+    boolean isSumTree(Node root) {
+        return sum(root)!=-1;
+    }
+}
