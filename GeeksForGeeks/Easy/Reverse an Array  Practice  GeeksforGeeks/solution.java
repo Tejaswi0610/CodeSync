4d59e27452d6fc1a4a9239d050b34bbda712ed7c
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-05-18 14:17:23 +0530

    Solved Reverse an Array | Practice | GeeksforGeeks

--- GeeksForGeeks/Easy/Reverse an Array  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Easy/Reverse an Array  Practice  GeeksforGeeks/solution.java
@@ -2,6 +2,7 @@ class Solution {
     public void reverseArray(int arr[]) {
         int start=0;
         int end=arr.length-1;
+        
         while(start<end){
             int temp=arr[start];
             arr[start]=arr[end];
