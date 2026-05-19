675c41ff9a45f9eca8d8969f109464cf7aec4654
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-05-19 16:14:37 +0530

    Solved Implement Trie (Prefix Tree)

---
+++ LeetCode/Medium/Implement Trie (Prefix Tree)/solution.java
@@ -0,0 +1,47 @@
+class Trie {
+    TrieNode root;
+    TrieNode[] childern;
+    boolean isEnd;
+    TrieNode(){
+        childern=new TrieNode[26];
+        isEnd=false;
+    }
+    public Trie() {
+        root = new TrieNode();
+    }
+    public void insert(String word) {
+        TrieNode curr=root;
+        for(char ch:word.toCharArray()){
+            if(curr.children[ch-'a']==null)
+                curr.children[ch-'a']=new TrieNode();
+            curr=curr.children[ch-'a'];
+        }
+        curr.isEnd=true;
+    }
+    public boolean search(String word) {
+        TrieNode curr=root;
+        for(char ch:prefix.toCharArray()){
+            if(curr.childrren[ch-'a']==null)
+                return false;
+            curr=curr.children[ch-'a'];
+        }
+        return curr.isEnd;
+    }
+    public boolean startsWith(String prefix) {
+        TrieNode curr=root;
+        for(char ch:word.toCharArray()){
+            if(curr.children[ch-'a']==null)
+                return false;
+            curr=curr.children[ch-'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
