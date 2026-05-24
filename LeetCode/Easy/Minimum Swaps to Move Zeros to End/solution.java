1aab9e578a414cf36f99073a6b2885538d15b26a
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-05-24 18:51:37 +0530

    Solved Minimum Swaps to Move Zeros to End

---
+++ LeetCode/Easy/Minimum Swaps to Move Zeros to End/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumSwaps(int[] nums) {
+        int n=nums.length;
+        int total=0;
+        for(int num:nums){
+            if(num==0)
+                total++;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
