3bd18b9c763b70da2f26d44a740cdb4a65e368f1
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-06-02 19:38:38 +0530

    Solved Find the Duplicate Number

--- LeetCode/Medium/Find the Duplicate Number/solution.java
+++ LeetCode/Medium/Find the Duplicate Number/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
         int ans=0;
-        for(int i=0;i<=nums.length;i++){
-            if(nums[i]==nums[i+1] && nums[i+1]<nums.length){
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1] ){
                 ans=nums[i];
+                break;
             }
         }
         return ans;
