36d736f60a3ba2fff10c6d5efd1053aa80d2585d
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-06-04 19:22:18 +0530

    Solved Maximum Subarray

--- LeetCode/Medium/Maximum Subarray/solution.java
+++ LeetCode/Medium/Maximum Subarray/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int n=nums.length;
-        int maxSum=Interger.MIN_VALUE;
+        int maxSum=Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-        }
-        if(sum>maxSum){
-            maxSum=sum;
-        }
-        if(sum<0){
-            sum=0;
+            if(sum>maxSum){
+                maxSum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
         }
         return maxSum;
     } 
