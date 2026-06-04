94ce451edc50bcf3cd70a2b217427d7486dc4b29
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-06-04 19:25:12 +0530

    Solved Maximum Product Subarray

---
+++ LeetCode/Medium/Maximum Product Subarray/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int product=1;
+        for(int i=0;i<nums.length){
+            product=product*max;
+            if(product>max){
+                max=product;
+            }
+            if(product<0){
+                product=0;
+            }
+        }
+        return max;
+    }
+}
