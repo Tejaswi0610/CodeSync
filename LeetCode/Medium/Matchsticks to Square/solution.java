2a2184055644eaa441fcd54d3933e5ce10c7d30d
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-07-13 22:11:44 +0530

    Solved Matchsticks to Square

---
+++ LeetCode/Medium/Matchsticks to Square/solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        for(int i=0;i<matchsticks.length;i++){
+            if(matchsticks[i]+matchsticks[])
+        }
+    }
+}
