27cfec0abea50b785c064d9fbd8f4e1a8c603642
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-08-19 18:25:39 +0530

    Solved Split a String in Balanced Strings

--- LeetCode/Easy/Split a String in Balanced Strings/solution.java
+++ LeetCode/Easy/Split a String in Balanced Strings/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        int count=0;
+        int count=0;;
         int balance=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='R'){
