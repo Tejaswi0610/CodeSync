c25c9ff3c9facd105a8cdb6606895c015dcde8f3
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-08-27 13:54:56 +0530

    Solved Distribute Candies

--- LeetCode/Easy/Distribute Candies/solution.java
+++ LeetCode/Easy/Distribute Candies/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        int ans=0;
-        if(n>0){
-            ans=n/2;
+       int ans=0;
+       for(int i=0;i<candyType.length;i++){
+        if(candyType.length>0){
+            ans=candyType.length/2;
         }
-        return ans;
+       }
+       return ans;
     }
 }
