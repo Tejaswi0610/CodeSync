28dd1186b68311775612536bdf3e43fd72db2c51
Tejaswi Bethoju <tejaswibethoju66@gmail.com>
2026-08-29 16:03:35 +0530

    Solved Different Ways to Add Parentheses

---
+++ LeetCode/Medium/Different Ways to Add Parentheses/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(ch=='+' || ch=='-' || ch=='*'){
+                List<Integer> left=diffWaysToCompute(s.substring(0,i));
+                List<Integer> right=diffWaysToCompute(s.substring(i+1));
+                for(int a:left){
+                    for(int b: right){
+                        if(ch=='+'){
+                            ans.add(a+b);
+                        }
+                        if(ch=='-'){
+                            ans.add(a-b);
+                        }
+                        if(ch=='*'){
+                            ans.add(a*b);
+                        }
+                    }
+                }
+            }
+        }
+        if(ans.isEmpty()){
+            ans.add(Integer.parseInt(s));
+        }
+        return ans;
+    }
+}
